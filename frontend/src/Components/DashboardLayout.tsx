93ebcaedab8166cb429c7879d02c92aaae0ec533
cedrotech1 <cedrotech1@gmail.com>
2025-01-18 20:58:05 +0200

    set_readme.md

--- frontend/src/Components/DashboardLayout.tsx
+++ frontend/src/Components/DashboardLayout.tsx
@@ -9,48 +9,53 @@ import instance from '@/API';
 // import NoBudgetPage from '@/Pages/NoBudgetPage';
 
 const LogedInLayout = () => {
-    const user = useSelector(getUser)
-    // const isBugdet = true
-    const dispatch = useDispatch()
-    const [loading, setLoading] = useState(false)
-    const budget = useSelector(getBudget)
-    const getUserBudget = async () => {
-        setLoading(true)
-        await instance.get('/budgets').then((res) => {
-            console.log('res-budget', res.data.data);
-            dispatch(res.data.data)
-            // setBudget(res.data.data)
-            console.log('budget', budget);
+  const user = useSelector(getUser);
+  // const isBugdet = true
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const budget = useSelector(getBudget);
+  const getUserBudget = async () => {
+    setLoading(true);
+    await instance
+      .get('/budgets')
+      .then((res) => {
+        console.log('res-budget', res.data.data);
+        dispatch(res.data.data);
+        // setBudget(res.data.data)
+        console.log('budget', budget);
+      })
+      .catch(() => {
+        // toast.error('Failed. Error getting user budget')
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  useEffect(() => {
+    getUserBudget();
+  }, []);
 
-        }).catch(() => {
-            // toast.error('Failed. Error getting user budget')
-        }).finally(() => {
-            setLoading(false)
-        })
-    }
-    useEffect(() => {
-        getUserBudget()
-    }, [])
-
-    if (!user) return <Navigate to="/auth/login" replace />;
-    if (user && budget === undefined) return <div>Loading...</div>;
-    if (user && !budget) return <Navigate to="/create-budget" replace />;
-    if (user && budget) return <div className='flex overflow-x-hidden bg-primary-backg'>
+  if (!user) return <Navigate to="/auth" replace />;
+  if (user && budget === undefined) return <div>Loading...</div>;
+  if (user && !budget) return <Navigate to="/create-budget" replace />;
+  if (user && budget)
+    return (
+      <div className="flex overflow-x-hidden bg-primary-backg">
         <div className={`w-[20vw] bg-black 'fixed z-20'}`}>
-            <SidebarNav />
+          <SidebarNav />
         </div>
         <div className={`w-[80vw] flex-1 bg-neutral-100`}>
-            <div className={`w-full  py-1 min-h-[90vh]`}>
-                <div style={{ zIndex: 10 }} className='sticky top-0 w-full mb-3 '>
-                    <DashboardTop />
-                </div>
-                <div className='px-2 md:px-8'>
-                    <Outlet />
-                </div>
+          <div className={`w-full  py-1 min-h-[90vh]`}>
+            <div style={{ zIndex: 10 }} className="sticky top-0 w-full mb-3 ">
+              <DashboardTop />
             </div>
+            <div className="px-2 md:px-8">
+              <Outlet />
+            </div>
+          </div>
         </div>
-    </div>
-
+      </div>
+    );
 };
 
 export default LogedInLayout;
